7317c8fcd94dc25588f8b43b33876c1eccdacd4a
iordanov.serhii <sergey.ior97@gmail.com>
2016-08-05 12:17:15 +0300

    add auth functionality to WEB layer

---
+++ uBlog/uBlog.WEB/App_Start/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using uBlog.Auth.BLL.Interfaces;
+using uBlog.Auth.BLL.Services;
+using uBlog.WEB;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace uBlog.WEB
+{
+    public class Startup
+    {
+        readonly IServiceCreator _serviceCreator = new ServiceCreator();
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<IUserService>(CreateUserService);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+
+        private IUserService CreateUserService()
+        {
+            return _serviceCreator.CreateUserService("BlogContext");
+        }
+    }
+}
---
+++ uBlog/uBlog.WEB/Controllers/AccountController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using uBlog.Auth.BLL.DTO;
+using uBlog.Auth.BLL.Infrastructure;
+using uBlog.Auth.BLL.Interfaces;
+using uBlog.WEB.Models;
+
+namespace uBlog.WEB.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IUserService UserService => 
+            HttpContext.GetOwinContext().GetUserManager<IUserService>();
+
+        private IAuthenticationManager AuthenticationManager => 
+            HttpContext.GetOwinContext().Authentication;
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel model)
+        {
+            await SetInitialDataAsync();
+            if (ModelState.IsValid)
+            {
+                var userDto = new UserDTO { Email = model.Email, Password = model.Password };
+                ClaimsIdentity claim = await UserService.Authenticate(userDto);
+                if (claim == null)
+                {
+                    ModelState.AddModelError("", "Invalid login or password");
+                }
+                else
+                {
+                    AuthenticationManager.SignOut();
+                    AuthenticationManager.SignIn(new AuthenticationProperties
+                    {
+                        IsPersistent = true
+                    }, claim);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            AuthenticationManager.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            await SetInitialDataAsync();
+            if (ModelState.IsValid)
+            {
+                UserDTO userDto = new UserDTO
+                {
+                    Email = model.Email,
+                    Password = model.Password,
+                    Name = model.Name,
+                    Role = "user"
+                };
+                OperationDetails operationDetails = await UserService.Create(userDto);
+
+                if (!operationDetails.Succedeed)
+                    ModelState.AddModelError(operationDetails.Property, operationDetails.Message);
+
+                return View("SuccessRegister");                    
+            }
+            return View(model);
+        }
+        private async Task SetInitialDataAsync()
+        {
+            await UserService.SetInitialData(new UserDTO
+            {
+                Email = "admin@gmail.com",
+                UserName = "admin@gmail.com",
+                Password = "admin123",
+                Name = "Admin Adminovich",
+                Role = "admin",
+            }, new List<string> { "user", "admin" });
+        }
+    }
+}
